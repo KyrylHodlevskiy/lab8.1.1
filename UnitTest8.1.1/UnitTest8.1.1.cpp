dd3653edfa3ec63c096637cb2825aa987b6e7a72
KyrylHodlevskiy <kn2004k@gmail.com>
2021-12-08 23:46:04 +0200

    Добавьте файлы проекта.

---
+++ UnitTest8.1.1/UnitTest8.1.1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\Users\Pavilion\Desktop\Універ\Алгоритмізація та програмування\лаб 8.1\lab8.1.4\lab8.1.4\lab8.1.4.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest811
+{
+	TEST_CLASS(UnitTest811)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char str[] = "abcsdiedabc";
+			int a = Itr(str);
+			Assert::AreEqual(a, 2);
+		}
+	};
+}
---
+++ lab8.1.4/lab8.1.4.cpp
@@ -0,0 +1,46 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+
+#include <iostream> 
+#include <ObjIdl.h>
+
+using namespace std;
+
+int Itr(char* str)
+{
+    int  count = 0;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if ((str[i] == 'b') && (str[i - 1] == 'a') && (str[i + 1] == 'c'))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+char* Replace(char* str)
+{
+    int i = 0;
+    for (i = 0; str[i] != '\0' && str[i + 1] != '\0' && str[i + 2] != '\0'; i++)
+    {
+        if ((str[i] == 'a' && str[i + 1] == 'b' && str[i + 2] == 'c'))
+        {
+            str[i] = '*';
+            str[i + 1] = '*';
+            str[i + 2] = ' ';
+        }
+    }
+    return str;
+}
+int main()
+{
+    char s[40];
+    char str[40];
+    cout << "Enter string:" << endl;
+    cin.getline(str, 40);
+    cout << endl << "Count abc =" << Itr(str) << endl;
+    char* changed = new char[40];
+    changed = Replace(str);
+    cout << "S=   " << changed << endl;
+}
